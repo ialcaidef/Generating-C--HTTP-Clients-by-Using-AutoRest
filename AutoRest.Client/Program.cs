7d0bf29cb0471df165179884c7842cd4563c7ded
Alcaide F. Inocente <ialcaidef@hotmail.com>
2021-01-17 13:43:58 +0100

    first commit

---
+++ AutoRest.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace AutoRest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (
+                sender, cert, chain, sslPolicyErrors) => {return true;};
+
+            MyAPI client = new MyAPI(new Uri("http://localhost:5000"), clientHandler);
+            IList<Destination> destinationList = client.ApiDestinationsGet();
+
+
+            Console.WriteLine("\nAll Destination");
+            foreach (Destination destination in destinationList)
+            {
+                Console.WriteLine($"{destination.CityName} - {destination.Airport}");
+            }
+            // ReadKey used that the console will not close when the code end to run.
+            Console.ReadKey();
+        }
+    }
+}
